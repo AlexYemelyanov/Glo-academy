b980f85c1a6a109a2a57e6ef05f4a821c4655300
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-04-01 20:23:01 +0300

    add some variables, write alert and some console.log

---
+++ script/script.js
@@ -0,0 +1,10 @@
+var money = 1000
+let income = 6;
+const addExpenses = 'Hellow';
+let deposit = 456;
+let mission = null;
+const period = 'World';
+
+alert('Здравствуйте Александр');
+console.log(money + '$');
+console.log(addExpenses + ' ' + period);
