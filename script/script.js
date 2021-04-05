2a066e6418aee03a82db7a1a030716878fdc71f9
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-04-05 22:53:48 +0300

    add code before lesson number 3

--- script/script.js
+++ script/script.js
@@ -1,10 +1,36 @@
-let money = 1000;
+let money = +prompt('Ваш месячный доход:');
 let income = 'Фриланс';
-let addExpenses = 'Квартира, машина, интернет, продукты';
-let deposit = true;
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую:');
+let deposit = confirm('Есть ли у вас депозит в банке?');
 let mission = 100000;
 let period = 12;
-let butgetDay = money / 30;
+let expenses1 = prompt('Введите обязательную статью расходов:');
+let amount1 = +prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов:');
+let amount2 = +prompt('Во сколько это обойдется?');
+let budgetMonth = money - (amount1 + amount2);
+let butgetDay = budgetMonth / 30;
+let ambition = Math.ceil(mission / budgetMonth);
+
+
+
+
+/*if (money >= 30000) {
+  message = 'У вас высокий уровень дохода';
+} else if (10000 == money < 30000) {
+  message = 'У вас средний уровень дохода';
+} else if (0 == money < 9999) {
+  message = 'У вас низкий уровень дохода';
+} else {
+  message = 'Что-то пошло не так';
+};
+*/
+
+let lvlIncome = (money >= 30000) ? 'У вас высокий уровень дохода!' :
+  (10000 <= money) ? 'У вас средний уровень дохода!' :
+  (0 <= money) ? 'У вас низкий уровень дохода!' :
+  'Что-то пошло не так';
+
 
 console.log(typeof money);
 console.log(typeof income);
@@ -13,4 +39,7 @@ console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
 console.log(addExpenses.toLowerCase().split(', '));
-console.log('Доход в день рублей: ' + Math.floor(butgetDay * 100) / 100);
+console.log('Бюджет на день: ' + Math.floor(butgetDay));
+console.log('Бюджет на месяц: ' + budgetMonth);
+console.log(lvlIncome);
+console.log('Цель будет достигнута за ' + ambition + ' месяц(-а/-ев)')
