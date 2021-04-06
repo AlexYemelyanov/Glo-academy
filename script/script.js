43b76cf5bb4af20fe46f6933c3ae2ffb84a11a56
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-04-06 18:59:17 +0300

    solved a hard task of lesson 4

--- script/script.js
+++ script/script.js
@@ -1,35 +1,10 @@
-let lang = prompt('Введите язык по примеру ru или eng');
-let langArray = [];
-let namePerson = prompt('Введите ваше имя:');
-let person;
-
-if (lang === 'ru') {
-  message = ('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-} else if (lang === 'eng') {
-  message = ('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+function getArgument(str) {
+  str = prompt('Введите любой текст: ');
+  str = str.trim();
+  if (!isNaN(str)) str += '  Это не строка!';
+  if (str.length <= 30) return alert(str);
+  str = str.slice(0, 30) + '...';
+  return alert(str);
 };
 
-switch (lang) {
-  case 'ru':
-    console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
-    break;
-  case 'eng':
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-    break;
-}
-
-
-langArray['ru'] = ['Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'];
-langArray['eng'] = ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'];
-
-
-person = (namePerson == 'Артем') ? 'Директор' :
-  (namePerson == 'Максим') ? 'Преподаватель' :
-  (namePerson == 'Александр') ? 'Преподаватель' :
-  'Студент';
-
-
-
-console.log(message);
-console.log(langArray[lang]);
-alert(person);
+getArgument();
