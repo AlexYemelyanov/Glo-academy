69da88bcd886b37cea0312c15080772dcd0cb83f
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-04-07 22:24:52 +0300

    fixed code

--- script/script.js
+++ script/script.js
@@ -1,10 +1,8 @@
 function getArgument(str) {
-  str = prompt('Введите любой текст: ');
-  str = str.trim();
-  if (!isNaN(str)) str += '  Это не строка!';
-  if (str.length <= 30) return alert(str);
+  if (typeof str != 'string') str += '  Это не строка!';
+  if (str.length <= 30) return alert(str = str.trim());
   str = str.slice(0, 30) + '...';
-  return alert(str);
+  return alert(str = str.trim());
 };
 
-getArgument();
+console.log(getArgument('     ghbdtn ijhgijgwpopwfqojbopqjpka'));
