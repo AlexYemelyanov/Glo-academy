eecdbba4d652a4029c852b79975b4d6ef91af95b
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-04-09 01:21:03 +0300

    some function in lesson06

--- script/script.js
+++ script/script.js
@@ -1,97 +1,41 @@
-let isNumber;
-
-let money;
-let income = 'Фриланс';
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую:');
-let deposit = confirm('Есть ли у вас депозит в банке?');
-let mission = 100000;
-let expenses = [];
-let getExpensesMonth;
-let getAccumulatedMonth;
-let accumulatedMonth;
-let butgetDay;
-let getTargetMonth;
-let ambition;
-let getStatusInCome;
-let showTypeOf;
-let expensesAmount;
-let start;
-
-isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n)
-};
+'use strict';
 
 
-start = function () {
+let x = 1 + (Math.random() * 100);
+x = Math.round(x);
 
-  do {
-    money = prompt('Ваш месячный доход:');
-  }
 
-  while (!isNumber(money))
 
-};
-start();
+//let guesseFaild = alert('');
 
-getStatusInCome = function () {
-  if (money >= 30000) {
-    return ('У вас высокий уровень дохода!');
-  } else if (10000 <= money) {
-    return ('У вас средний уровень дохода!');
-  } else if (0 <= money) {
-    return ('У вас низкий уровень дохода!');
+
+function comparsion() {
+  let guesseCount = 1;
+  let userGuesse = +prompt('Введите число!');
+  let att = confirm('Попытка № ' + guesseCount);
+
+  //let faild = alert(guesseFaild);
+
+  if (userGuesse == x) {
+    return alert('Вы угадали!');
+
+
   } else {
-    return ('Что-то пошло не так');
+    if (userGuesse > x) {
+      alert('Загаданное число меньше!')
+    }
+    if (userGuesse < x) {
+      alert('Загаданное число больше!')
+    }
   }
-};
-
-showTypeOf = function (data) {
-  console.log(typeof (data));
-};
-
-getExpensesMonth = function () {
-  let sum = 0;
-
-  for (let i = 0; i < 2; i++) {
-
-    expenses[i] = prompt('Введите обязательную статью расходов:');
-
-    sum += +prompt('Во сколько это обойдется?');
-
-    while (!isNumber(sum)) {
-      sum += +prompt('Во сколько это обойдется?');
-    };
+  if (guesseCount != 10) {
+    comparsion();
+  } else if (guesseCount === 10) {
+    alert('Game Over!');
   }
+  guesseCount++
+  //guesseFaild = userGuesse + '';
+}
 
-  return sum;
-};
-
-expensesAmount = getExpensesMonth();
-
-getAccumulatedMonth = function () {
-  return money - expensesAmount;
-};
-
-accumulatedMonth = getAccumulatedMonth();
-
-butgetDay = accumulatedMonth / 30;
-
-getTargetMonth = function () {
-  if (Math.ceil(mission / accumulatedMonth) > 0)
-    return 'Цель будет достигнута за ' + Math.ceil(mission / accumulatedMonth) + ' месяц(-а, -ев)';
-  else if (Math.ceil(mission / accumulatedMonth) < 0)
-    return 'Цель не будет достигнута!';
-};
-
-ambition = getTargetMonth();
-
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-console.log(expensesAmount);
-console.log('Цель заработать ' + mission + ' рублей');
-console.log(addExpenses.toLowerCase().split(', '));
-console.log('Бюджет на день: ' + Math.floor(butgetDay));
-console.log('Бюджет на месяц: ' + accumulatedMonth);
-console.log(ambition);
-console.log(getStatusInCome());
+comparsion();
+console.log(x);
