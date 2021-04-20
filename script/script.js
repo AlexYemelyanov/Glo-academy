f45dab7fad81691129ae486a8f80a1dfa73b3277
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-04-20 18:42:06 +0300

    started

--- script/script.js
+++ script/script.js
@@ -1,19 +1,1 @@
 'use strict';
-
-
-let randomizer = document.getElementById('change'),
-  textIns = document.getElementById('color');
-
-
-
-
-
-function randomNum() {
-  let rndNumber = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-  textIns.innerHTML = rndNumber;
-
-  document.body.style.backgroundColor = textIns.textContent;
-};
-
-
-randomizer.addEventListener('click', randomNum);
