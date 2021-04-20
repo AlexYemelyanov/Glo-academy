6c55ad7ed9e1a2e5ad8e5afe079657a4d2c67743
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-04-20 20:26:57 +0300

    start lesson

--- script/script.js
+++ script/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function DomElement(selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+}
+
+DomElement.prototype.create = function () {
+  if (this.selector.startsWith('.')) {
+    let div = document.createElement('div');
+    div.className = (this.selector);
+    return div;
+  }
+  if (this.selector.startsWith('#')) {
+    let p = document.createElement('p');
+    p.idName(this.selector);
+  }
+};
+
+let check = new DomElement(".formula");
+console.log(check.create());
