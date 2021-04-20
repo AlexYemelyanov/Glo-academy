7cf2b1186d973e6b50df648c376b80a79276877f
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-04-20 17:44:18 +0300

    completed hard task lesson 13

--- script/script.js
+++ script/script.js
@@ -1,76 +1,19 @@
-function getNoun(number, one, two, five) {
-  let n = Math.abs(number);
-  n %= 100;
-  if (n >= 5 && n <= 20) {
-    return five;
-  }
-  n %= 10;
-  if (n === 1) {
-    return one;
-  }
-  if (n >= 2 && n <= 4) {
-    return two;
-  }
-  return five;
-}
+'use strict';
 
-function newTypeOfDate() {
-  let date = new Date();
 
-  let weekday = date.getDay();
-  console.log(weekday);
-  let options = {
-    weekday: 'long'
-  };
-  let tdDay = new Intl.DateTimeFormat('ru-RU', options).format(date);
-  let day = date.getDate();
-  let month = date.getMonth();
-  let monthArr = [
-    'января',
-    'февраля',
-    'марта',
-    'апреля',
-    'мая',
-    'июня',
-    'июля',
-    'августа',
-    'сентября',
-    'октября',
-    'ноября',
-    'декабря',
-  ];
-  let year = date.getFullYear();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-  return 'Сегодня ' + tdDay + ',' + day + ' ' + monthArr[month] + ' ' + year + ' года, ' + hours + ' ' + getNoun(hours, 'час', 'часа',
-    'часов') + ' ' + minutes + ' ' + getNoun(minutes, 'минута', 'минуты', 'минут') + ' ' + seconds + ' ' + getNoun(seconds, 'секунда', 'секунды', 'секунд')
-
-}
-setInterval(function () {
-  document.getElementById('current_date_time').innerHTML = newTypeOfDate();
-}, 1000);
+let randomizer = document.getElementById('change'),
+  textIns = document.getElementById('color');
 
 
 
-function zeroFirstFormat(value) {
-  if (value < 10) {
-    value = '0' + value;
-  }
-  return value;
+
+
+function randomNum() {
+  let rndNumber = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+  textIns.innerHTML = rndNumber;
+
+  document.body.style.backgroundColor = textIns.textContent;
 };
 
-function dateTime() {
-  let currentDatetime = new Date();
-  let day = zeroFirstFormat(currentDatetime.getDate());
-  let month = zeroFirstFormat(currentDatetime.getMonth() + 1);
-  let year = currentDatetime.getFullYear();
-  let hours = zeroFirstFormat(currentDatetime.getHours());
-  let minutes = zeroFirstFormat(currentDatetime.getMinutes());
-  let seconds = zeroFirstFormat(currentDatetime.getSeconds());
 
-  return day + '.' + month + '.' + year + ' ' + hours + ':' + minutes + ':' + seconds;
-};
-setInterval(function () {
-  document.getElementById('current_date_time_block').innerHTML = dateTime();
-}, 1000);
+randomizer.addEventListener('click', randomNum);
