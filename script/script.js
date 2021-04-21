602ec5edaf4b03c2e08c8e8ab57185528dffd0d5
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-04-21 12:15:58 +0300

    Completed hard task lesson 014

--- script/script.js
+++ script/script.js
@@ -1,12 +1,13 @@
 'use strict';
 let body = document.getElementsByTagName('body');
 
-function DomElement(selector, height, width, bg, fontSize) {
+function DomElement(selector, height, width, bg, fontSize, position) {
   this.selector = selector;
   this.height = height;
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
+  this.position = position;
 }
 
 DomElement.prototype.create = function () {
@@ -17,6 +18,7 @@ DomElement.prototype.create = function () {
     width: ${this.width};
     background: ${this.bg};
     font-size: ${this.fontSize};
+    position: ${this.position};
     `;
     div.innerHTML = '<h1>Привет</h1>';
     return div;
@@ -29,12 +31,50 @@ DomElement.prototype.create = function () {
     width: ${this.width};
     background: ${this.bg};
     font-size: ${this.fontSize};
+    position: ${this.position};
     `;
     p.textContent = 'Шалом!';
     return p;
   }
 };
 
-let check = new DomElement("#formula", "100px", "100px", "red", "20px");
+let check = new DomElement(".formula", "100px", "100px", "red", "10px", "absolute");
 let pageInsert = check.create();
 document.body.appendChild(pageInsert);
+document.addEventListener('DOMContentLoaded', function () {
+  alert('Dom completed');
+
+});
+
+function moveRect(event) {
+
+  let formula = document.querySelector(".formula");
+
+  let compStyle = window.getComputedStyle(formula);
+
+  let left = parseInt(compStyle.marginLeft);
+  let top = parseInt(compStyle.marginTop);
+
+  switch (event.key) {
+
+    case "ArrowLeft":
+      if (left > 0)
+        formula.style.marginLeft = left - 10 + 'px';
+      break;
+    case "ArrowUp":
+      if (top > 0)
+        formula.style.marginTop = top - 10 + 'px';
+      break;
+    case "ArrowRight":
+      if (left < document.documentElement.clientWidth - 100)
+        formula.style.marginLeft = left + 10 + 'px';
+      break;
+    case "ArrowDown":
+      if (top < document.documentElement.clientHeight - 100)
+        formula.style.marginTop = top + 10 + 'px';
+      break;
+  }
+  console.log(event.key);
+}
+
+addEventListener("keydown", moveRect);
