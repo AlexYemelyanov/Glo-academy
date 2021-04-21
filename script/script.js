839d66e88f5c73a4faf95eabab27fb064c63a863
Alexey Emelyanov <a.emelyanov291189@gmail.com>
2021-04-21 09:29:11 +0300

    completed lesson_014 first part

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 'use strict';
+let body = document.getElementsByTagName('body');
 
 function DomElement(selector, height, width, bg, fontSize) {
   this.selector = selector;
@@ -11,14 +12,29 @@ function DomElement(selector, height, width, bg, fontSize) {
 DomElement.prototype.create = function () {
   if (this.selector.startsWith('.')) {
     let div = document.createElement('div');
-    div.className = (this.selector);
+    div.className = (this.selector.substring(1));
+    div.style.cssText = `height: ${this.height};
+    width: ${this.width};
+    background: ${this.bg};
+    font-size: ${this.fontSize};
+    `;
+    div.innerHTML = '<h1>Привет</h1>';
     return div;
   }
   if (this.selector.startsWith('#')) {
     let p = document.createElement('p');
-    p.idName(this.selector);
+    p.id = (this.selector.substring(1));
+    p.style.cssText = `
+    height: ${this.height};
+    width: ${this.width};
+    background: ${this.bg};
+    font-size: ${this.fontSize};
+    `;
+    p.textContent = 'Шалом!';
+    return p;
   }
 };
 
-let check = new DomElement(".formula");
-console.log(check.create());
+let check = new DomElement("#formula", "100px", "100px", "red", "20px");
+let pageInsert = check.create();
+document.body.appendChild(pageInsert);
